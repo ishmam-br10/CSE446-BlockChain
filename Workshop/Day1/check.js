e3817217d2e94fe19b978138d1afd04dad8e3e3b
Ishmam Bin Rofi <ishmam.bin.rofi1@g.bracu.ac.bd>
2025-02-20 12:59:36 +0600

    double eq and triple eq explaination

---
+++ Workshop/Day1/check.js
@@ -0,0 +1,14 @@
+if (5 == '5'){
+    console.log('both are equal')
+}
+else{
+    console.log('both are not equal')
+}
+
+console.log("three equal signs")
+if (5 === '5'){
+    console.log('both are equal')
+}
+else{
+    console.log('both are not equal')
+}
---
+++ Workshop/Day1/new.py
@@ -0,0 +1,4 @@
+if (5 == '5'):
+    print('True')
+else:
+    print('False')
