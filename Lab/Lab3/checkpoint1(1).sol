1940a5a9726203249e57c41c0d1c305629bfa7b9
Ishmam Bin Rofi <ishmam.bin.rofi1@g.bracu.ac.bd>
2025-03-06 14:13:32 +0600

    checkpoint1.1

---
+++ Lab/Lab3/checkpoint1(1).sol
@@ -0,0 +1,16 @@
+pragma solidity >= 0.8.2 <0.9.0;
+// Create a function that takes your studentID of integer value and returns your name. 
+//If you enter the ID of any other student, it should return “This is not your ID”. 
+// now make the student id sting and compare again
+contract Checkpoint1 {
+    string private studentID = "24241332";
+    string private name = "Ishmam Bin Rofi";
+
+    function StudentName(string memory _studentID) public view returns (string memory) {
+        if (keccak256(abi.encodePacked(_studentID)) == keccak256(abi.encodePacked(studentID))) {
+            return name;
+        } else {
+            return "This is not your ID";
+        }
+    }
+}
