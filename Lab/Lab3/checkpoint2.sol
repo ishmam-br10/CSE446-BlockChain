ea357ff8fd34cbc8153c3fce7c71ff28f645888d
Ishmam Bin Rofi <ishmam.bin.rofi1@g.bracu.ac.bd>
2025-03-06 14:26:53 +0600

    checkpoint 2 done

---
+++ Lab/Lab3/checkpoint2.sol
@@ -0,0 +1,17 @@
+//Write a smart contract that stores student id and the name of the student. 
+//Also create a separate function that takes studentID and returns student name. 
+//[ You must use mapping ]
+
+pragma solidity >= 0.8.2 <0.9.0;
+
+contract Checkpoint2 {
+    mapping(uint256 => string) private studentDatabase;
+    
+    function AddStudent(uint256 _studentID, string memory _name) public {
+        studentDatabase[_studentID] = _name;
+    }
+    
+    function StudentName(uint256 _studentID) public view returns(string memory) {
+        return studentDatabase[_studentID];
+    }
+}
