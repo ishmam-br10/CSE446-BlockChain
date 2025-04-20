0c3a28efd51ce3188f4e37b24b4ba0acea7c379e
Ishmam Bin Rofi <ishmam.bin.rofi1@g.bracu.ac.bd>
2025-04-20 09:27:18 +0600

    project removed for now

--- Lab/LabProjectFull/election/migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-// requiring the contract
-var Election = artifacts.require("./Election.sol");
-
-// exporting as module 
- module.exports = function(deployer) {
-  deployer.deploy(Election);
- };
-
